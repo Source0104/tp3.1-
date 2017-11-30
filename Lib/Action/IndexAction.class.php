8b0dc53be51a91abe611674053575ccd0ec25a0e
Source <296476710@qq.com>
2017-11-30 21:24:23 +0800

    添加前台首页和对应的js,css

---
+++ Lib/Action/IndexAction.class.php
@@ -0,0 +1,31 @@
+<?php
+/*****
+	Zhaolong 2017/11/21
+	联系方式 QQ：296476710
+******/
+class IndexAction extends Action {
+    public function index(){			/***首页***/
+        $g=M('basic');
+        $i=$g->where('id=1')->find();
+        $this->assign('data',$i);
+        $this->display('index');
+    }
+    public function contact(){			/**联系我们**/
+      $this->display();
+    }
+    public function about(){			/****关于我们****/
+       $g=M('basic');
+        $i=$g->where('id=1')->find();
+        $this->assign('data',$i);
+      $this->display();
+    }
+    public function products(){			/**产品展示**/
+      $this->display();
+    }
+    public function short(){
+      $this->display();
+    }
+    public function typography(){
+      $this->display();
+    }
+}
